c8ece2a0e78c42542c3fff695ddd4e02b87c1265
Maggie Ward <mlward639@gmail.com>
2021-07-08 17:56:29 -0400

    fillReadme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# unit-nine-homework
+# unit-nine-homework{
+	"title": "title",
+	"description": "describe",
+	"instal": "enter here",
+	"usage": "usage here",
+	"contribution": "guidelines",
+	"confirm": "test",
+	"license": "A"
+}{
+	"title": "r",
+	"description": "w",
+	"instal": "d",
+	"usage": "s",
+	"contribution": "a",
+	"confirm": "f",
+	"license": "A"
+}
--- index.js
+++ index.js
@@ -27,34 +27,73 @@ const questions = [
       },
       {
         type: 'input',  
+        message: 'List any collaborators or third-party assets used on this project.',
+        name: 'credits',
+      },
+      {
+        type: 'input',  
         message: 'What are your contribution guidelines?',
         name: 'contribution',
       },
       {
         type: 'input',
         message: 'Enter your test instructions.',
-        name: 'confirm',
+        name: 'test',
       },
       {
         type: 'list',
         message: "Choose your license.",
         name: "license",
-        choices: ['A', 'B', 'C'], //complete
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'none'], 
     }
 ];
+
+const fillReadme = (res) => 
+`# ${res.title}
+<br>
+<br>
+## Description
+${res.description}
+<br>
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contribution Guidelines](#contribution-guidelines)
+- [License](#license)
+<br>
+## Installation
+${res.instal}
+<br>
+## Usage
+${res.usage} 
+## Credits
+${res.credits}
+<br>
+## Contribution Guidelines
+${res.contributions}
+<br>
+## Test Instructions
+${res.test}
+<br>
+## License
+${res.license}
+<br>
+## Badges
+`
 
 //console.log (questions)
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile(fileName, JSON.stringify(data, null, '\t'), (err) => {err ? console.error(err) : console.log('logged')})
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) => {err ? console.error(err) : console.log('logged')})
 }
-writeToFile("README.md", res)
+//writeToFile("README.md", res)
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(writeToFile("README.md", data))
-}
+    .then(writeToFile('README.md', res)) 
+    }
 
 // Function call to initialize app
-//init();
+init();
